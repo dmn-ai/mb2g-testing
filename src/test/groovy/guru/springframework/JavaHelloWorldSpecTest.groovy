eacdf8ff19a84ac3302705df1022a56cc8786ab8
Brian Martin <bm@palaucir.cloud>
2026-01-03 16:20:15 -0500

    My Changes.

--- src/test/groovy/guru/springframework/JavaHelloWorldSpecTest.groovy
+++ src/test/groovy/guru/springframework/JavaHelloWorldSpecTest.groovy
@@ -2,14 +2,8 @@ package guru.springframework
 
 import spock.lang.Specification
 
-
-/**
- * Created by jt on 2018-12-12.
- */
 class JavaHelloWorldSpecTest extends Specification {
-
     def "GetHello"() {
-
         when:
         def foo = "123"
 
