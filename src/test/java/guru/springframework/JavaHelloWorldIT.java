9fe9191cd2a565f85b5d3af0ef71387b13c572b6
Brian Martin <bm@palaucir.cloud>
2026-01-03 15:46:00 -0500

    My changes.

--- src/test/java/guru/springframework/JavaHelloWorldIT.java
+++ src/test/java/guru/springframework/JavaHelloWorldIT.java
@@ -2,14 +2,10 @@ package guru.springframework;
 
 import org.junit.jupiter.api.Test;
 
-/**
- * Created by jt on 2018-12-14.
- */
 public class JavaHelloWorldIT {
 
     @Test
     void myFauxIntegrationTest() {
-
-        System.out.println("My IT Ran");
+        System.out.println( "My IT Ran..." );
     }
 }
