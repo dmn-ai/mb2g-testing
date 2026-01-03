57e10dc12c11cd5a331e187767644f6d04a807bc
Brian Martin <bm@palaucir.cloud>
2026-01-03 16:35:42 -0500

    My Changes.

--- src/test/java/guru/springframework/JUnit5FauxTest.java
+++ src/test/java/guru/springframework/JUnit5FauxTest.java
@@ -2,13 +2,11 @@ package guru.springframework;
 
 import org.junit.jupiter.api.Test;
 
-/**
- * Created by jt on 2018-12-09.
- */
-public class JUnit5FauxTest {
+class JUnit5FauxTest {
 
     @Test
-    void someTestforJUnit5() {
-        System.out.println("I Ran....");
+    void someTestForJUnit5() {
+        System.out.println( "I ran..." );
     }
+
 }
