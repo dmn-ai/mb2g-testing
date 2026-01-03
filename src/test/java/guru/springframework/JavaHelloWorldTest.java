f699906ad5224aa3cb062d9a8a029df397e7f9ef
Brian Martin <bm@palaucir.cloud>
2026-01-03 16:34:00 -0500

    My Changes.

--- src/test/java/guru/springframework/JavaHelloWorldTest.java
+++ src/test/java/guru/springframework/JavaHelloWorldTest.java
@@ -2,14 +2,13 @@ package guru.springframework;
 
 import org.junit.Test;
 
-import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.*;
 
 public class JavaHelloWorldTest {
 
     @Test
     public void getHello() {
         JavaHelloWorld javaHelloWorld = new JavaHelloWorld();
-
-        assertEquals("Hello World", javaHelloWorld.getHello());
+        assertEquals( "Hello World", javaHelloWorld.getHello());
     }
 }
